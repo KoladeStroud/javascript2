154d08572348de8991b992e0fee8f8e48b53b808
Ragaz <adeyanjukolawole70@gmail.com>
2022-06-23 19:29:32 -0700

    this is my second javascript assignment

---
+++ ass.js
@@ -0,0 +1,11 @@
+ let num1 = 10
+ let num2 = 20
+ let addition = num1 + num2
+ let division = num1/num2
+ let subtraction = num1 - num2
+ let multiplication = num1 * num2
+
+ console.log(addition)
+ console.log(division)
+ console.log(subtraction)
+ console.log(multiplication)
